07d9c17da1ed0cdb01bca8b7129717c480802da3
jai0323 <jaishobhit45@gmail.com>
2025-05-14 19:12:17 +0530

    Simple Calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include "./bits/stdc++.h"
+#define int long long
+using namespace std;
 
-int main() {
-    std::cout << "Jai!!";
+void solve(){
+   
+
+}
+
+signed main() {
+    // breaking link  between cin &scanf and  cout & printf for fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int _t = 1;
+
+    // cin >> _t;
+    while(_t--){
+        solve();
+    }
+
     return 0;
 }
