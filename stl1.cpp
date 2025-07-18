8e5d5195b755d3c1a1cc38d661dd92fc124f6462
jai0323 <jaishobhit45@gmail.com>
2025-07-18 23:21:38 +0530

    18/07/2025

---
+++ stl1.cpp
@@ -0,0 +1,32 @@
+#include <./bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve(){
+
+    int arr[]={1,7,6,4,5,3,2,8,9};
+
+    sort(arr,arr+9);
+
+    for(int i = 0; i < 9; i++)
+        cout<< arr[i] << "  ";
+
+    // we are sub arr from lower bound bcoz it return pointer of the index 
+    cout << "\nlower bound position => " << lower_bound(arr, arr+9, 6) - arr << endl;
+    cout << "\nupper bound position => " << upper_bound(arr, arr+9, 6) - arr << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int _t=1;
+
+    // cin >> _t;
+
+    while(_t--){
+        solve();
+    }
+    return 0;
+}
